4100011b8eb98e28c7c59d07c4d429f4d60636c5
kaiky de Moura <kaikymoura972@gmail.com>
2025-02-13 16:10:23 -0300

    Fixing function get_data retun

---
+++ app/services/nba_api/nba_client.py
@@ -0,0 +1,14 @@
+from nba_api.stats.static import players, teams
+
+def get_active_players(limit):
+    limit == None 
+    return players.get_active_players()
+
+def get_player_by_last_name(name):
+    return players.find_players_by_last_name(name)
+
+def get_all_teams():
+    return teams.get_teams()
+
+def get_team_by_name(abbreviation):
+    teams.find_team_by_abbreviation(abbreviation)
--- app/services/scrapper.py
+++ app/services/scrapper.py
@@ -7,55 +7,47 @@ WEBSITES = [
         "address": "https://www.espn.com/nba/",
         "base": "https://www.espn.com",
         "selector": ".headlineStack__header + section > ul > li > a",
+        "selectorImage": 'img'
     },
     {
         "name": "bleacher_report",
         "address": "https://bleacherreport.com/nba",
         "base": "",
         "selector": ".articleTitle",
+        "selectorImage": 'img'
     },
      {
     "name": 'slam',
     "address": 'https://www.slamonline.com/',
     "base": '',
     "selector": '.h-bloglist-block-content-top > h3 > a',
-  },
-  {
-    "name": 'yahoo',
-    "address": 'https://sports.yahoo.com/nba/?guccounter=1',
-    "base": 'https://sports.yahoo.com',
-    "selector": '.js-content-viewer',
-  },
-  {
-    "name": 'nba',
-    "address": 'https://www.nba.com/news/category/top-stories',
-    "base": 'https://www.nba.com',
-    "selector": '.flex-1 > a',
+    "selectorImage": 'img'
   },
 ]
 
 NBA_WEBSITES = [
-  {
-    "name": 'nba',
-    "address": 'https://www.nba.com/news/category/top-stories',
-    "base": 'https://www.nba.com',
-    "selectorUrl": '.ArticleTile_tileMainContent__c_bU1 > a',
-    "selectorTitle":
-      '.ArticleTile_tileMainContent__c_bU1 > a > header > h3 > span ',
-  },
-  {
-    "name": 'nba_canada',
-    "address": 'https://www.sportingnews.com/ca/nba/news',
-    "base": 'https://www.sportingnews.com',
-    "selectorUrl": '.list-item__title > a',
-    "selectorTitle": '.list-item__title > a',
-  },
+    {
+        "name": 'nba',
+        "address": 'https://www.nba.com/news/category/top-stories',
+        "base": 'https://www.nba.com',
+        "selectorUrl": '.ArticleTile_tileMainContent__c_bU1 > a',
+        "selectorTitle": '.ArticleTile_tileMainContent__c_bU1 > a > header > h3 > span',
+        "selectorImage": '.ArticleTile_tileImage__no39y'
+    },
+    {
+        "name": 'nba_canada',
+        "address": 'https://www.sportingnews.com/ca/nba/news',
+        "base": 'https://www.sportingnews.com',
+        "selectorUrl": '.list-item__title > a',
+        "selectorTitle": '.list-item__title > a',
+        "selectorImage": '.ArticleTile_tileImage__no39y'
+    },
 ]
 
 """ 
-Web scrapper to retrieve news/articles about basketballl from different websites
+Web scrapper to retrieve news/articles about basketball from different websites
 
-Inpired by the following sources:
+Inspired by the following sources:
 
     https://github.com/kevinn03/nba_api
 
@@ -63,22 +55,47 @@ Inpired by the following sources:
 
 """
 
-
 # Get data from a website
 def get_data(website):
     try:
         articles = []
-        response = requests.get(website["address"])
+        session = requests.Session()
+        session.headers.update({
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+        })
+        response = session.get(website["address"])
+        
+        if response.status_code != 200:
+            print(f"Failed to retrieve data from {website['name']}, status code {response.status_code}")
+            return []
+        
         html = response.text
         soup = BeautifulSoup(html, "html.parser")
 
-        for element in soup.select(website["selector"]):
+        elements = soup.select(website["selector"])
+        image_elements = soup.select(website["selectorImage"])
+
+        for i, element in enumerate(elements):
             title = element.get_text(strip=True)
             url = element.get("href")
+            
             if url and not url.startswith("http"):
                 url = website["base"] + url
+
+            image_url = None
+            if i < len(image_elements):
+                img_element = image_elements[i]
+                if img_element.get("src"):
+                    image_url = img_element["src"]
+                elif img_element.get("srcset"):
+                    image_url = img_element["srcset"].split(",")[0].split(" ")[0]
+        
+            
             if title:
-                articles.append({"title": title, "url": url, "source": website["name"]})
+                articles.append({"title": title, 
+                                 "url": url, 
+                                 "source": website["name"], 
+                                 "image": image_url})
         
         # Filter out empty titles
         return [article for article in articles if article["title"] != ""]
@@ -96,12 +113,22 @@ def get_nba_data(website):
         nba_articles = []
         nba_title = [title.get_text(strip=True) for title in soup.select(website["selectorTitle"])]
         nba_url = [url.get("href") for url in soup.select(website["selectorUrl"])]
+        
+        image_elements = soup.select(website["selectorImage"])
 
         for i in range(len(nba_title)):
+            image_url = None
+            
+            if i < len(image_elements):
+                img_element = image_elements[i].select_one('img') 
+                if img_element and img_element.get("src"):
+                    image_url = img_element["src"]
+            
             nba_articles.append({
                 "title": nba_title[i],
                 "url": website["base"] + nba_url[i],
-                "source": website["name"]
+                "source": website["name"],
+                "image": image_url
             })
         
         return nba_articles
