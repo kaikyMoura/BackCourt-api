7512dcb24a75e8cf9cb747e07bd83888c883d795
Kaiky Tupinambá <kaikymoura972@gmail.com>
2025-04-17 15:51:46 -0300

    feat(config): NBA custom headers configuration
    - Adding custom headers for the nba api.

---
+++ app/config/nba_api_config.py
@@ -0,0 +1,17 @@
+from nba_api.stats.library.http import NBAStatsHTTP
+
+
+def configure_nba_api_headers():
+    NBAStatsHTTP._NBAStatsHTTP__headers = {
+        "Host": "stats.nba.com",
+        "Connection": "keep-alive",
+        "Accept": "application/json, text/plain, */*",
+        "x-nba-stats-origin": "stats",
+        "x-nba-stats-token": "true",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "Origin": "https://www.nba.com",
+        "Referer": "https://www.nba.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+    }
--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
+from app.config.nba_api_config import configure_nba_api_headers
 from app.routes import router
 from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Backcourt API", description="API for the Backcourt application", version="1.0")
+app = FastAPI(
+    title="Backcourt API",
+    description="API for the Backcourt application",
+    version="1.0",
+)
+
+configure_nba_api_headers()
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,7 +20,7 @@ app.add_middleware(
     allow_headers=["X-Custom-Header"],
 )
 
-app.include_router(router, prefix='/api/v1')
+app.include_router(router, prefix="/v1")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
